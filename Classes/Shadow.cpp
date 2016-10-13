afacf710ec952c638201733509ceafcf8d8aa830
smiger <530509325@qq.com>
2016-10-13 10:42:58 +0800

    smiger commit

--- Classes/Shadow.cpp
+++ Classes/Shadow.cpp
@@ -9,7 +9,7 @@ bool Shadow::init()
 	auto bg = Sprite::create("bk.jpg");
 	bg->setPosition(size.width/2,size.height/2);
 	this->addChild(bg);
-	//直接投影在当前png图像上
+	//1、直接投影在当前png图像上
 	//auto sprite = Sprite::create("fish.png");  
 	//sprite->setPosition(size.width/2, size.height /2 );  
 
@@ -19,17 +19,42 @@ bool Shadow::init()
 	//sprite->setGLProgram(shader_program); 
 	//this->addChild(sprite);
 
+	////2、拷贝进行投影
+	//auto sprite = Sprite::create("fish.png");  
+	//sprite->setPosition(size.width/2, size.height /2 );  
+
+	//auto shader_program = GLProgram::createWithFilenames("shadow.vsh", "shadow.fsh");  
+	//shader_program->use();  
+	//shader_program->setUniformsForBuiltins();
+
+	//auto fishClone = Sprite::create("fish.png");  
+	//
+	//fishClone->setGLProgram(shader_program);
+	//fishClone->setPosition(-10.f, -10.f);
+	//fishClone->setAnchorPoint(Vec2(0,0));
+	//fishClone->setScale(0.8f);
+	//sprite->addChild(fishClone, -1); 
+	//this->addChild(sprite);
+
+	//3、以上创建的GLProgram可能在android上跑不出来，
+	//原因是shader_program返回的是空值，可用以下方法解决
+
+	auto shader_program = new CCGLProgram();  
+	shader_program->retain();  
+	shader_program->initWithVertexShaderFilename("shadow.vsh", "shadow.fsh");  
+
+	shader_program->link();  
+	shader_program->updateUniforms();
+
 
 	auto sprite = Sprite::create("fish.png");  
-	sprite->setPosition(size.width/2, size.height /2 );  
+	sprite->setPosition(size.width/2, size.height /2 ); 
 
-	auto shader_program = GLProgram::createWithFilenames("shadow.vsh", "shadow.fsh");  
-	shader_program->use();  
-	shader_program->setUniformsForBuiltins();
-
-	auto fishClone = Sprite::createWithSpriteFrame(sprite->getSpriteFrame());
-	fishClone->setPosition(10, 5); 
+	auto fishClone = Sprite::create("fish.png");  
 	fishClone->setGLProgram(shader_program);
+	fishClone->setPosition(-10.f, -10.f);
+	fishClone->setAnchorPoint(Vec2(0,0));
+	fishClone->setScale(0.8f);
 	sprite->addChild(fishClone, -1); 
 	this->addChild(sprite);
 
